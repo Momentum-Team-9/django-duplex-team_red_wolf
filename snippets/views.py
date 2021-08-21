31e49ebba09a36a5d79bd87590c5b1d24c210871
sarah-wines <sarah.lodico.wines@gmail.com>
2021-08-21 11:51:20 -0400

    functioning registration redux

--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import User
 
 # Create your views here.
+def index(request):
+    users = User.objects.all()
+    return render(request, 'snippets/index.html', {'users': users})
