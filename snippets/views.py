52728046950a61e2397e1a1ccfd56eedf5829251
sarah-wines <sarah.lodico.wines@gmail.com>
2021-08-24 20:15:02 -0400

    gravatar connects to user email for individual profile view

--- snippets/views.py
+++ snippets/views.py
@@ -35,7 +35,7 @@ def profile(request):
 def user_profile(request, username):
    user = get_object_or_404(User, username=username)
    snippets = user.snippets.filter(public=True)
-   return render(request, "snippets/user_profile.html", {"snippets": snippets, "username": username})
+   return render(request, "snippets/user_profile.html", {"snippets": snippets, "username": username, "user": user})
 
 @login_required
 def add_snippet(request):
