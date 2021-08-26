00b2fbcd549e5a3219b2edc5b1bd73ade3ce66ce
sarah-wines <sarah.lodico.wines@gmail.com>
2021-08-26 12:43:46 -0400

    fixes gravatar bug

--- snippets/views.py
+++ snippets/views.py
@@ -35,11 +35,10 @@ def profile(request):
 def user_profile(request, username):
     user = get_object_or_404(User, username=username)
     snippets = user.snippets.filter(public=True)
-
     return render(
         request,
         "snippets/user_profile.html",
-        {"snippets": snippets, "username": username},
+        {"snippets": snippets, "username": username, "user": user},
     )
 
 
